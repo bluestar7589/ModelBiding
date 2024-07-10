cb444ae7e919a220d94bb88e883c7daa7198f76f
bluestar7589 <bluestar7589@yahoo.com>
2024-07-09 20:15:15 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelBiding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
