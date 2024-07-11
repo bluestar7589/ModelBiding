224c4bbafd3c5d00ad81f0f64aa85860e8b1ad98
bluestar7589 <bluestar7589@yahoo.com>
2024-07-10 20:48:56 -0700

    Initial website using asp.net MVCAdd project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBiding.Models;
+
+namespace ModelBiding.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Get: /Product/Create
+        [HttpGet] // This action is hit when the user navigates to the page
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Post: /Product/Create
+        [HttpPost] // This action is hit after the user submit form data
+        public IActionResult Create(IFormCollection form)
+        {
+            // Validate all data
+
+            Products p = new()
+            {
+                Title = form["Title"],
+                Price = double.Parse(form["Price"]),
+                Quantity = int.Parse(form["Quantity"])
+            };
+            
+
+            // Add to database
+            //ProductDb.Add(p);
+            // Return a view
+
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpGet]
+        public IActionResult CreateWithModelBiding() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateWithModelBiding(Products p) {
+            // Validate all data
+            if (ModelState.IsValid) {
+                // Add to database
+                //ProductDb.Add(p);    
+                return RedirectToAction("Index", "Home");
+            }
+            // If the data is not valid, return the same page with same data - error message
+            return View(p);
+            
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBiding.Models
+{
+    public class Products
+    {
+        /// <summary>
+        /// The title of the product
+        /// </summary>
+        [Required(ErrorMessage = "Need to input the title !!!")] //  the error message when the title is empty
+        [Display(Name = "Product Name")] // Change the display name for the label of title
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The selling price of product
+        /// </summary>
+        //[Required(ErrorMessage = "Price is not valid!!!")]
+        [Range(0,double.MaxValue,ErrorMessage ="Please input the valid price !!!")]
+        [DataType(DataType.Currency )]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The number of unit in stock
+        /// </summary>
+        [Range(0,double.MaxValue)]
+        [Display(Name="Qty")]
+        public int Quantity { get; set; }
+    }
+}
